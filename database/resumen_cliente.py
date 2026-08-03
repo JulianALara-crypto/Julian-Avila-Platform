0d83d6da70ad75ad7296daff11647f4d0bc5ceda
JulianALara-crypto <a.operaciones@prindel.com.co>
2026-08-03 16:15:23 -0500

    resumen_cliente.py

--- database/resumen_cliente.py
+++ database/resumen_cliente.py
@@ -1,1 +1,47 @@
+import pandas as pd
 
+from database.medidas import obtener_medidas_cliente
+from database.gimnasio import buscar_cliente_gym
+
+
+
+def resumen_cliente(cedula):
+
+
+    datos = {}
+
+
+
+    cliente = buscar_cliente_gym(
+        cedula
+    )
+
+
+    if not cliente.empty:
+
+        datos["cliente"] = cliente.iloc[0]
+
+
+
+    medidas = obtener_medidas_cliente(
+        cedula
+    )
+
+
+
+    if not medidas.empty:
+
+
+        medidas = medidas.sort_index()
+
+
+        datos["inicial"] = medidas.iloc[0]
+
+        datos["actual"] = medidas.iloc[-1]
+
+
+        datos["historial"] = medidas
+
+
+
+    return datos
